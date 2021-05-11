7703812005931a1b8535c8c1c734e7e7d142e6ab
CHWu <wuchin1997@gmail.com>
2021-05-11 18:42:21 +0200

    add composant

---
+++ composant/Taches.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text , TouchableOpacity } from 'react-native';
+
+const test_DATA 
+
+
+export default function Taches (params) {
+    return {
+
+    }
+}
